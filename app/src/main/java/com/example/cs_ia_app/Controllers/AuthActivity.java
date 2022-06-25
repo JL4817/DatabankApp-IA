548c632f7034fb1d981a22877f71dcbf26adc997
JL4817 <89381622+JL4817@users.noreply.github.com>
2022-06-25 14:10:13 +0200

    app doesn't lauch

--- app/src/main/java/com/example/cs_ia_app/Controllers/AuthActivity.java
+++ app/src/main/java/com/example/cs_ia_app/Controllers/AuthActivity.java
@@ -1,14 +1,23 @@
 package com.example.cs_ia_app.Controllers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.cs_ia_app.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AuthActivity extends AppCompatActivity {
@@ -20,15 +29,11 @@ public class AuthActivity extends AppCompatActivity {
     private EditText emailField;
     private EditText passwordField;
 
-    //spinner
-    private LinearLayout layout;
-    private EditText nameField;
-    private Spinner userRoleSpinner;
-    private String selectedRole;
+    private String userId;
 
     //teacher
 
-    //student//           232
+    //student
 
 
     @Override
@@ -41,6 +46,73 @@ public class AuthActivity extends AppCompatActivity {
     }
 
 
+    public void logIn(View v){
+
+        String emailString = emailField.getText().toString();
+        String passwordString = passwordField.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(emailString, passwordString)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("SIGN UP", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+
+                            Intent nextScreen = new Intent(getBaseContext(), MainLobby.class);
+                            startActivity(nextScreen);
+
+                        } else {
+
+                            // If sign in fails, display a message to the user.
+                            Log.w("SIGN UP", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(AuthActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+
+
+    public void signUp(View v){
+
+        String emailString = emailField.getText().toString();
+        String passwordString = passwordField.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d("Sign Up", "Succesfully signed up the user");
+
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+
+                }else{
+                    Log.w("Sign up", "User Creation has failed", task.getException());
+                    updateUI(null);
+                }
+            }
+        });
+
+
+    }
+
+
+
+        public void updateUI(FirebaseUser currentUser){
+
+            if(currentUser != null){
+                Intent intent = new Intent(this, MainLobby.class);
+                startActivity(intent);
+            }
+
+
+        }
 
 
 
---
+++ app/src/main/java/com/example/cs_ia_app/Controllers/MainLobby.java
@@ -0,0 +1,16 @@
+package com.example.cs_ia_app.Controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cs_ia_app.R;
+
+public class MainLobby extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_lobby);
+    }
+}
