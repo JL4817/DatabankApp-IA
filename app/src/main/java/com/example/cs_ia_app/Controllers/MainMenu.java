055d5838d82dd7f036d52f236b08c7e5571e8a9e
JL4817 <89381622+JL4817@users.noreply.github.com>
2022-08-17 13:04:38 +0200

    user can sign out

--- app/src/main/java/com/example/cs_ia_app/Controllers/MainMenu.java
+++ app/src/main/java/com/example/cs_ia_app/Controllers/MainMenu.java
@@ -1,19 +1,32 @@
 package com.example.cs_ia_app.Controllers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.cs_ia_app.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainMenu extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_lobby);
+
+        mAuth = FirebaseAuth.getInstance();
+        firestore = FirebaseFirestore.getInstance();
     }
 
 
@@ -42,6 +55,22 @@ public class MainMenu extends AppCompatActivity {
         startActivity(nextScreen);
     }
 
+
+    public void signUserOut(View v){
+
+        mAuth.signOut();
+
+        showToast("User Logged Out");
+
+        Intent nextScreen = new Intent(getBaseContext(), AuthActivity.class);
+        startActivity(nextScreen);
+
+    }
+
+
+    private void showToast(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
 
 
 
