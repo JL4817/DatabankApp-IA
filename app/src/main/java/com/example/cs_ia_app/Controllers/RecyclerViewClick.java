3a40bb0463cb064857e9a29367530018edd894d1
JL4817 <89381622+JL4817@users.noreply.github.com>
2022-08-18 19:48:48 +0200

    update item work in progress1

--- app/src/main/java/com/example/cs_ia_app/Controllers/RecyclerViewClick.java
+++ app/src/main/java/com/example/cs_ia_app/Controllers/RecyclerViewClick.java
@@ -20,14 +20,19 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
-
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class RecyclerViewClick extends AppCompatActivity implements View.OnClickListener {
@@ -41,6 +46,8 @@ public class RecyclerViewClick extends AppCompatActivity implements View.OnClick
     private ImageView imageView;
     private TextView tvlocation, tvname, tvID, tvitemLink;
     private String location, name, ID, link, image;
+
+    private TextView tvName2, tvLink2, tvLocation2;
 
     private ArrayList<Item> itemList;
     private int position;
@@ -56,7 +63,6 @@ public class RecyclerViewClick extends AppCompatActivity implements View.OnClick
 
         mAuth = FirebaseAuth.getInstance();
         firestore = FirebaseFirestore.getInstance();
-
 
 
         if (getIntent().hasExtra("itemList") && getIntent().hasExtra("itemPos")) {
@@ -110,11 +116,56 @@ public class RecyclerViewClick extends AppCompatActivity implements View.OnClick
 
            // imageView.setImageURI(image);
 
+
+
+
+
         }
+
+        tvName2 = findViewById(R.id.lvName3);
+        tvLocation2 = findViewById(R.id.lvLocation3);
+        tvLink2 = findViewById(R.id.lvLink3);
+
+
+
 
 
     }
 
+
+    public void newInfoUpdate(View v){
+
+/*
+
+        String newName = tvName2.getText().toString();
+        String newLocation = tvLocation2.getText().toString();
+        String newLink = tvLink2.getText().toString();
+
+
+        CollectionReference applicationsRef = firestore.collection("items");
+        DocumentReference applicationIdRef = applicationsRef.document(selected.getItemID());
+        applicationIdRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    List<Map<String, Object>> users = (List<Map<String, Object>>) document.get(selected.getItemID());
+                }
+            }
+        });
+
+ */
+
+
+        firestore.collection("item")
+                .document("frank")
+                .update({
+                        "age": 13,
+                "favorites.color": "Red"
+                });
+
+
+
+    }
 
 
 
